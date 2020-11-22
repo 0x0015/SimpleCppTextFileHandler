81d7ec51ffb909b40cb2df7b91e9130230179359
0x0015 <64482595+0x0015@users.noreply.github.com>
2020-11-21 21:02:07 -0500

    added file.hpp

---
+++ file.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string readFile(string filename){
+
+  std::ifstream ifs(filename);
+  std::string content( (std::istreambuf_iterator<char>(ifs) ),
+                       (std::istreambuf_iterator<char>()    ) );
+
+  return(content);
+}
+
+void writeFile(string filename, string content){
+	ofstream file;
+	file.open(filename);
+	file << content;
+	file.close();
+	return;
+}
